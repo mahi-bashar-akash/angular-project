00862e14b7825338bea79568d53ce152dcccacab
mahibashardeveloper <mahibashar2023@gmail.com>
2024-06-03 15:48:25 +0600

    update changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { BlogComponent } from './blog/blog.component';
-import { ServiceComponent } from './service/service.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'blog', component: BlogComponent },
-  { path: 'service', component: ServiceComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'settings', component: SettingsComponent },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+
 export class HomeComponent {
+
+  userCompany: string = 'Redishketch';
+  userEducationalQualification: string = 'Bachelor of Business Administration ( Computer Application )';
+  userProgrammingExperience: string = 'HTML, CSS, JavaScript (TypeScript) , PHP, MySQL.';
+  userFrameworkDevelopment: string = 'PHP ( Laravel ), Javascript ( Vue, Next, Angular )';
+  userName: string = 'Mahi Bashar Akash';
+  userEmail: string = 'mahibashar2023@gmail.com';
+  uniqueNumber: string = '+880';
+  userPhoneNumber: string = '01400125289';
+  userPresentAddress: string = 'Bashtolla, Mollapara, Jessore Sadar, Jessore, Bangladesh';
+
+  contactParam = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
+
+  contactForm(event: any) {
+    console.log(event.target)
+  }
 
 }
