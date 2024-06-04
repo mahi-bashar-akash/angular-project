9b95119e97b86aa42a2715320a6e2c2d37bc5fea
mahibashardeveloper <mahibashar2023@gmail.com>
2024-06-04 09:18:42 +0600

    update some changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { FormsModule } from "@angular/forms";
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  email: string = 'mahibashar2023@gmail.com';
+  phone: string = '+880 01645820007';
+  address: string = 'Mollapara Bashtola, Jessore Sadar, Jessore, Bangladesh';
 
   contactParam = {
     name: '',
