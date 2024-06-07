51f7e1bdbe9c943c46453cd49f56234b24e533b3
mahibashardeveloper <mahibashar2023@gmail.com>
2024-06-07 19:16:45 +0600

    update changes modal attachment

--- src/app/admin/calendar/calendar.component.ts
+++ src/app/admin/calendar/calendar.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-    imports: [
-        NgForOf
-    ],
+    imports: [],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
+
+  openEventModal() {
+    const myModal = new bootstrap.Modal("#eventModel", {keyboard: false});
+    myModal.show();
+  }
+
+  closeEventModal() {
+    let myModalEl = document.getElementById('eventModel');
+    let modal = bootstrap.Modal.getInstance(myModalEl)
+    modal.hide();
+  }
+
+  openDeleteEventModal() {
+    const myModal = new bootstrap.Modal("#eventModel", {keyboard: false});
+    myModal.show();
+  }
+
+  closeDeleteEventModal() {
+    let myModalEl = document.getElementById('eventModel');
+    let modal = bootstrap.Modal.getInstance(myModalEl)
+    modal.hide();
+  }
 
 }
